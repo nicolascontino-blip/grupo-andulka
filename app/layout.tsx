87646f64a92c151ecb1e2d4b91af29f62c942d7e
Nicolas <nicolascontino@gmail.com>
2026-04-20 19:14:56 -0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,26 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Open_Sans } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const openSans = Open_Sans({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "600"],
 });
 
 export const metadata = {
   title: "GRUPO ANDULKA",
   description: "Arquitectura corporativa",
-}; 
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="es">
+      <body className={openSans.className}>
+        {children}
+      </body>
     </html>
   );
 }
-import { Open_Sans } from "next/font/google";
-
-const openSans = Open_Sans({ subsets: ["latin"] });
-
-<body className={openSans.className}></body>
